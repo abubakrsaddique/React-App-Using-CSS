b783b689474222d6d9f6d54a7d8c94c24ab37004
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2024-04-17 21:36:51 +0500

    first commit

---
+++ vite-project/src/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Icon } from "@mui/material";
+import { LocationOn, Phone, Email } from "@mui/icons-material";
+import "./header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-item">
+        <Icon>
+          <LocationOn />
+        </Icon>
+        <span>Rezilla, 18 Grattan St, Brooklyn </span>
+      </div>
+
+      <div className="header-item">
+        <Icon>
+          <Phone />
+        </Icon>
+        <span>+1 206-214-229 </span>
+      </div>
+
+      <div className="header-item">
+        <Icon>
+          <Email />
+        </Icon>
+        <span>support@rezilla.com </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
