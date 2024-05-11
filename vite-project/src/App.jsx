24b428abc8862fbba614bd2c4cce3939e1f9e42f
Abubakar9894 <abubakarsaddique786.tsk@gmail.com>
2024-05-11 23:46:19 +0500

    19th commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -12,9 +12,9 @@ function App() {
       <Header />
       <Navbar />
       <RealEstateSection />
-      {/* <CardComponent />
+      <CardComponent />
       <MarqueeComponent />
-      <About /> */}
+      <About />
     </>
   );
 }
